1bc7ba0b1f170711e13eee84990480368af289cb
Jilian Lubrat <jilian.lubrat@etu.univ-cotedazur.fr>
2023-10-13 17:35:50 +0200

    New Game Method start

--- src/poker/Game.java
+++ src/poker/Game.java
@@ -1,4 +1,6 @@
 package poker;
+import java.text.ParseException;
+import java.util.Scanner;
 
 /**
  * A game
@@ -10,5 +12,15 @@ public class Game {
 
     public Game(int handSize) {
         this.handSize = handSize;
+    }
+
+
+    public void start() throws IllegalArgumentException, ParseException{
+        System.out.print("Main 1:");
+        Scanner hand1 = new Scanner(System.in);
+        System.out.print("Main 2:");
+        Scanner hand2 = new Scanner(System.in);
+        Card[] handOne = Hand.parse(hand1.nextLine(),handSize);
+        Card[] handTwo = Hand.parse(hand1.nextLine(),handSize);
     }
 }
