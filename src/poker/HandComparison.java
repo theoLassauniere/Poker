31e1028d20e716586c5614be2a29bc517a9ad151
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2023-11-06 15:00:07 +0100

    Adding of full in HandComparison and GameTest

--- src/poker/HandComparison.java
+++ src/poker/HandComparison.java
@@ -20,18 +20,17 @@ public record HandComparison(int compareResult, Patterns pattern, List<Value> va
                 .append(compareResult() > 0 ? 1 : 2)
                 .append(" gagne avec ");
         switch (pattern()) {
-            case HIGHER -> stringRes.append("la carte la plus haute : ").append(values().get(0));
-            case PAIR -> stringRes.append("une paire de : ").append(values().get(0));
-            case DOUBLE_PAIR ->
-                    stringRes.append("une double paire dont la paire la paire gagnante est paire de : ").append(values().get(0));
-            case THREE_OF_A_KIND -> stringRes.append("un brelan de : ").append(values().get(0));
-            case FOUR_OF_A_KIND -> stringRes.append("un carré de : ").append(values().get(0));
-            case STRAIGHT -> stringRes.append("une suite dont la carte la plus haute est : ").append(values().get(0));
-            case COLOR -> stringRes.append("une couleur dont la carte la plus haute est : ").append(values().get(0));
-            case STRAIGHTFLUSH ->
-                    stringRes.append("une quinte flush dont la carte la plus haute est : ").append(values().get(0));
+            case HIGHER -> stringRes.append("la carte la plus haute : ");
+            case PAIR -> stringRes.append("une paire de : ");
+            case DOUBLE_PAIR -> stringRes.append("une double paire dont la paire la paire gagnante est paire de : ");
+            case THREE_OF_A_KIND -> stringRes.append("un brelan de : ");
+            case FOUR_OF_A_KIND -> stringRes.append("un carré de : ");
+            case STRAIGHT -> stringRes.append("une suite dont la carte la plus haute est : ");
+            case COLOR -> stringRes.append("une couleur dont la carte la plus haute est : ");
+            case STRAIGHTFLUSH -> stringRes.append("une quinte flush dont la carte la plus haute est : ");
+            case FULL -> stringRes.append("un full contenant un brelan de : ");
             default -> stringRes.append(pattern);
         }
-        return stringRes.toString();
+        return stringRes.append(values().get(0)).toString();
     }
 }
--- test/poker/GameTest.java
+++ test/poker/GameTest.java
@@ -98,7 +98,10 @@ class GameTest {
             "7Ca 8Ca 9Ca JCa ACa;8Co 9Co 10Co JCo ACo;La main 2 gagne avec la carte la plus haute : 10",
             "ACa KCa QCa JCa 10Ca;2Co 8Pi 10Co 10Tr 10Pi;La main 1 gagne avec une quinte flush dont la carte la plus haute est : A",
             "ACa KCa QCa JCa 10Ca;2Co 3Co 4Co 5Co 6Co;La main 1 gagne avec une quinte flush dont la carte la plus haute est : A",
-            "7Pi 8Pi 9Pi 10Pi JPi;8Ca 9Ca 10Ca JCa QCa;La main 2 gagne avec une quinte flush dont la carte la plus haute est : Q"
+            "7Pi 8Pi 9Pi 10Pi JPi;8Ca 9Ca 10Ca JCa QCa;La main 2 gagne avec une quinte flush dont la carte la plus haute est : Q",
+            "KCo KCa KPi 7Ca 7Co;ACa ACo APi 6Ca 6Co;La main 2 gagne avec un full contenant un brelan de : A",
+            "JCa JCo JPi 2Ca 2Co;8Co 8Ca 8Pi ACa APi;La main 1 gagne avec un full contenant un brelan de : J",
+
     }, delimiter = ';')
     void fiveCardGameTest(String firstHand, String secondHand, String output) {
         gameTest(fiveCardGame, firstHand, secondHand, output);
